62870bdec62b570070ba5851cb3d4534e30f7c65
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-10-31 16:06:43 -0700

    Create framework for Game and Player class, in main and testing file

---
+++ lib/connect_four.rb
@@ -0,0 +1,11 @@
+# Player class is used to create player 1 and player 2
+class Player
+  def initialize
+  end
+end
+
+# Game class is used to create the Game board and methods
+class Game
+  def initialize
+  end
+end
---
+++ spec/connect_four_rspec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/connect_four'
+
+describe Game do
+end
+
+describe Player do
+end
