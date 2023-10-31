eeebb81dac8a85b9b4cee6675bf6b8e9d1d64300
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-10-31 16:12:43 -0700

    Create subject for Game and Player class and add initialize testing template (no test required)

--- spec/connect_four_rspec.rb
+++ spec/connect_four_rspec.rb
@@ -1,7 +1,17 @@
 require_relative '../lib/connect_four'
 
 describe Game do
+  subject(:game_main) { described_class.new }
+
+  describe '#initialize' do
+    # Initialize -> No test necessary when only creating instance variables.
+  end
 end
 
 describe Player do
+  subject(:player_main) { described_class.new }
+
+  describe '#initialize' do
+    # Initialize -> No test necessary when only creating instance variables.
+  end
 end
