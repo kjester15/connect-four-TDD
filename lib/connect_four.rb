50adbc4a70b6288a10d9ae157bdcb9149f4bbe86
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-11-02 15:32:17 -0700

    Update player variables to hashes

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,39 +1,38 @@
 # Game class is used to create the Game board and methods
 class Game
-  attr_accessor :board_array, :player1_name, :player1_symbol, :player2_name, :player2_symbol, :game_finished
+  attr_accessor :board_array, :player1, :player2, :game_finished, :player_turn
 
   def initialize(board_array = Array.new(6) { Array.new(7) { ' ' } })
     # https://www.theodinproject.com/lessons/ruby-nested-collections
     # created immutable nested array for board_array
     @board_array = board_array
-    @player1_name = ''
-    @player2_name = ''
-    @player1_symbol = ''
-    @player2_symbol = ''
+    @player_turn = ''
+    @player1 = { name: '', symbol: '' }
+    @player2 = { name: '', symbol: '' }
     @game_finished = false
   end
 
   def greeting_setup
     puts 'Hello! Welcome to Connect Four. Let\'s play a game in the console.'
     puts 'What is player 1\'s name?'
-    @player1_name = gets.chomp
+    @player1[:name] = gets.chomp
     puts 'What is player 1\'s symbol?'
-    @player1_symbol = gets.chomp
-    until @player1_symbol.length == 1
+    @player1[:symbol] = gets.chomp
+    until @player1[:symbol].length == 1
       puts 'Please choose a letter that is only 1 character long.'
-      @player1_symbol = gets.chomp
+      @player1[:symbol] = gets.chomp
     end
     puts 'What is player 2\'s name?'
-    @player2_name = gets.chomp
+    @player2[:name] = gets.chomp
     puts 'What is player 2\'s symbol?'
-    @player2_symbol = gets.chomp
-    until @player2_symbol.length == 1 && @player2_symbol != @player1_symbol
+    @player2[:symbol] = gets.chomp
+    until @player2[:symbol].length == 1 && @player2[:symbol] != @player1[:symbol]
       puts 'Please choose a letter that is only 1 character long, and has not been chosen by Player 1.'
-      @player2_symbol = gets.chomp
+      @player2[:symbol] = gets.chomp
     end
     puts "You'll take turns choosing a column to 'drop' your piece into, choosing a number between 1 and 7. Get 4 in \
 a row and you win!"
-    puts "Here is your board - #{@player1_name}, you go first!"
+    puts "Here is your board - #{@player1[:name]}, you go first!"
   end
 
   def print_board
